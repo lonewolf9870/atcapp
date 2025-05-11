dd47103ac44f89c1a74e81db8727517c8737b79d
tharun <tharunakkineni9@gmail.com>
2025-05-12 03:39:55 +0530

    install whitenoise

--- atcbackend/atcbackend/settings.py
+++ atcbackend/atcbackend/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',  # Add whitenoise
+    'whitenoise.runserver_nostatic',
     'contact',
     'rest_framework',
     'corsheaders',

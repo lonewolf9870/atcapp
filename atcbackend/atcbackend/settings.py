0dc35300ccc55f3108010b557a92e426fffd4fae
tharun <tharunakkineni9@gmail.com>
2025-05-11 19:19:54 +0530

    made some changes for app

--- atcbackend/atcbackend/settings.py
+++ atcbackend/atcbackend/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contact',
+    'contact', 
     'rest_framework',
     'corsheaders',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- atcbackend/atcbackend/wsgi.py
+++ atcbackend/atcbackend/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for atcbackend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from waitress import serve
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atcbackend.settings')
 
 application = get_wsgi_application()
+
+if __name__ == "__main__":
+    serve(application, host='0.0.0.0', port=8000)
